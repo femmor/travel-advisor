1cb66406e2a2c6a5ea4efc8e138fb6dd8424a191
Emmanuel Egomson <fegomson@gmail.com>
2022-01-14 17:01:48 -0600

    Implement GoogleMapReact

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles.js';
 
 const Map = () => {
-  return (
-    <div>
-      <h1>Map</h1>
-    </div>
-  )
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
 
-export default Map
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBf46WKn4guuh7MbWFe7RaF6S8MyMGuI4E" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={5}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+        
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
