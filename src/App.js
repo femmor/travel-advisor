e8dd34c7c3201011b8fd85184636f921110af520
Emmanuel Egomson <fegomson@gmail.com>
2022-01-14 15:40:12 -0600

    Basic structure with Mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+// Components
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 const App = () => {
-  return <div>App component</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid
+        container
+        spacing={3}
+        style={{
+          width: "100%",
+        }}
+      >
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./styles.js"
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
